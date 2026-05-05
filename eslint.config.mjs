28f19262a7cba21c4f01db6595b3b0b1e99e2ec6
koreahghg <koreahghg@gmail.com>
2026-05-06 01:24:45 +0900

    style: prettier 적용으로 코드 스타일 통일

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,7 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettierConfig,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
